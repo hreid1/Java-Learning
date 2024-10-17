6d8e55849fc47620e36606aa690f4bcde2035fae
hreid1 <henryreid05@gmail.com>
2024-10-17 14:17:12 +0100

    Added basic examples for data types and control flow

---
+++ Basics/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int number = 10;
+        double decimal = 5.5;
+        char letter = 'A';
+        String message = "Hello, Java!";
+
+        System.out.println("Integer: " + number);
+        System.out.println("Double: " + decimal);
+        System.out.println("Character: " + letter);
+        System.out.println("String: " + message);
+    }
+}
+
